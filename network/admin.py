a0ba4792c7583c449db28b36de06e7dc758e53a8
GABRI Mate <gabrimate@duosol.hu>
2012-06-20 12:53:04 +0200

    Host ordering and admin actions.
    Host ordering by Entry.
    Enable/Disable Host and Enable/Disable WiFi actions.

--- network/admin.py
+++ network/admin.py
@@ -1,8 +1,47 @@
 from django.contrib import admin
 from network.models import Host
+from django.utils.translation import ugettext as _
 
 class HostAdmin(admin.ModelAdmin):
 	list_display = ( 'entry', 'host', 'ip', 'wifi', 'mac', 'enabled' )
 	list_filter = ( 'wifi', 'enabled' )
+	ordering = ( 'entry', )
+	actions = [ 'set_enabled', 'unset_enabled', 'set_wifi', 'unset_wifi' ]
+	
+	def set_enabled(self, request, queryset):
+		rows_updated = queryset.update(enabled=True)
+		if rows_updated == 1:
+			message_bit = _("1 host was")
+		else:
+			message_bit = _("%s hosts were") % rows_updated
+		self.message_user(request, "%s enabled" % message_bit)
+	set_enabled.short_description = _("Enable host")
+	
+	def unset_enabled(self, request, queryset):
+		rows_updated = queryset.update(enabled=False)
+		if rows_updated == 1:
+			message_bit = _("1 host was")
+		else:
+			message_bit = _("%s hosts were") % rows_updated
+		self.message_user(request, "%s disabled" % message_bit)
+	unset_enabled.short_description = _("Disable host")
+	
+	def set_wifi(self, request, queryset):
+		rows_updated = queryset.update(wifi=True)
+		if rows_updated == 1:
+			message_bit = _("1 host was")
+		else:
+			message_bit = _("%s hosts were") % rows_updated
+		self.message_user(request, "%s modified" % message_bit)
+	set_wifi.short_description = _("Enable WiFi")
+	
+	def unset_wifi(self, request, queryset):
+		rows_updated = queryset.update(wifi=False)
+		if rows_updated == 1:
+			message_bit = _("1 host was")
+		else:
+			message_bit = _("%s hosts were") % rows_updated
+		self.message_user(request, "%s modified" % message_bit)
+	unset_wifi.short_description = _("Disable WiFi")
 	
 admin.site.register(Host, HostAdmin)
